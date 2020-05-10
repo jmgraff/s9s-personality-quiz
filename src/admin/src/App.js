9d33e3342132528d7155ac79e7956c7112b8ec75
john <johnmgraffjr@gmail.com>
2020-05-10 11:34:34 -0400

    Results added to admin

--- src/admin/src/App.js
+++ src/admin/src/App.js
@@ -1,71 +1,6 @@
 import React from 'react';
 import Questions from './Questions.js';
-
-/*
-   window.quizData = {
-title: 'Which 90 Day Fiance Couple Are You?',
-description: 'Find out which couple you are with this super cool quiz!',
-questions: 
-[
-{ 
-question: 'Pick an outfit',
-answers: 
-[
-{ answer: 'Whatever is clean', value: 'am' },
-{ answer: 'Jeans and a T-Shirt', value: 'ba' },
-{ answer: 'Dressy Casual', value: 'aj' },
-{ answer: 'Slutty and Skin-tight', value: 'dj' }
-]
-},
-{ 
-question: 'Time of Day',
-answers: 
-[
-{ answer: 'Morning', value: 'am' },
-{ answer: 'Afternoon', value: 'ba' },
-{ answer: 'Evening', value: 'aj' },
-{ answer: 'Late night fun', value: 'dj' }
-]
-},
-{ 
-question: 'What are we doing?',
-answers: 
-[
-{ answer: 'Taking a walk through nature', value: 'ba' },
-{ answer: 'Catching live music', value: 'am' },
-{ answer: 'Dancing at a club', value: 'dj' }
-]
-},
-{ 
-question: 'What are we eating?',
-answers: 
-[
-{ answer: 'Fancy dinner', value: 'dj' },
-{ answer: 'Quick snack from a street vendor', value: 'ba' },
-{ answer: 'Ice cream', value: 'am' },
-{ answer: 'Who needs food?', value: 'aj' }
-]
-},
-{ 
-question: 'Goodnight kiss?',
-answers: 
-[
-{ answer: 'Yes', value: 'am' },
-{ answer: 'No', value: 'ba' },
-{ answer: 'Why stop there?', value: 'aj' }
-]
-},
-],
-results: 
-[
-{ title: 'Angela and Michael', value: 'am' },   
-{ title: 'Layrissa and Coltee', value: 'lc' },   
-{ title: 'Benjamin and Akeni', value: 'ba' },   
-{ title: 'Darcy and Jesse', value: 'dj' },   
-{ title: 'Ashley and Jay', value: 'aj' }
-]
-}
- */
+import Results from './Results.js';
 
 class App extends React.Component {
     constructor() {
@@ -109,6 +44,13 @@ class App extends React.Component {
         this.setState(newState);
     }
 
+    handleResultsChange(data) {
+        console.log(this.state);
+        let newState = {...this.state};
+        newState.quizData.results = data;
+        this.setState(newState);
+    }
+
     render() {
         return (
             <div className="App">
@@ -126,6 +68,10 @@ class App extends React.Component {
                         onChange={e => this.handleDescriptionChange(e)} 
                     />
                 </div>
+                <Results 
+                    data={this.state.quizData.results} 
+                    onChange={data => this.handleResultsChange(data)} 
+                />
                 <Questions 
                     data={this.state.quizData.questions} 
                     onChange={data => this.handleQuestionsChange(data)} 
